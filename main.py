9bc0c8dea0988d2fa49de00dafee7650f198e95a
rherik <kurtzgk@gmail.com>
2023-09-12 11:16:01 -0300

    adds .txt file for pip install

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from reactpy.backend.flask import configure
 from flask import Flask
 
 @component
-def hello_world():
-    return html.h1("Hello, World!")
+def PrintButton(display_text, message_text):
+    def handle_event(event):
+        print(message_text)
+    return html.button({"on_click": handle_event}, display_text)
+
+@component
+def button():
+    return html.div(
+                PrintButton("Play", "Playing"),
+                PrintButton("Pause", "Paused"),
+            )
 
 app = Flask(__name__)
-configure(app, hello_world)
+configure(app, button)
